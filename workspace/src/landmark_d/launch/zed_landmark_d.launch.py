f86fae06004cff98798b8f75cea10b3e2d9ca1cb
Ahmed Al-Hindawi <a.al-hindawi@ucl.ac.uk>
2024-06-26 15:25:40 +0100

    added setupudev.sh and changed landmarks image topics to correct ones

--- workspace/src/landmark_d/launch/zed_landmark_d.launch.py
+++ workspace/src/landmark_d/launch/zed_landmark_d.launch.py
@@ -4,17 +4,17 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # zed_launch,
-        Node(
-            package='zed_wrapper',
-            namespace='face1',
-            executable='camera.launch.py'
-        ),
+        # Node(
+        #     package='zed_wrapper',
+        #     namespace='face1',
+        #     executable='camera.launch.py'
+        # ),
         Node(
             package='face_d',
             namespace='face1',
             executable='face_d',
             name='face_detector',
-            remappings=[("/camera", "/zed/zed_node/rgb/image_rect_color"),
+            remappings=[("/image_rect", "/zed/zed_node/rgb/image_rect_color"),
                         ("/faces", "/faces")]
         ),
         Node(
